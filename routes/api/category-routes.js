3d83c7caf2be7c2028935dc1146288c878f6ebad
DistantDig <jameswaller711@gmail.com>
2023-11-02 15:31:29 -0700

    added get by id request to category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,15 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {

109ad24157a420fac53f9037fcec5a191360134c
DistantDig <jameswaller711@gmail.com>
2023-11-03 11:00:15 -0700

    added delete request for products that also deletes associated productTags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,8 +107,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productTagData = await ProductTag.destroy({
+      where: {
+        product_id: req.params.id
+      }
+    });
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
